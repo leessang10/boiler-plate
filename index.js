16bc3a51b9111126f507895e81cf1f7535e61fb5
leessang10 <leessang10@naver.com>
2022-05-31 18:17:52 +0900

    [+] config 파일 mongoDB 키값 은닉화 개발/배포 환경 분리

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const {User} = require('./models/User');
+const bodyParser = require('body-parser');
+const config = require('./config/key');
 const app = express();
 const port = 3000;
 
+// Content-Type: application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+// Content-Type: application/json
+app.use(express.json());
+
+
 // mongoDB 연결
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://leessang10:WooJoo4032!@mongotest.qwjs5.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log("mongoDB Connected"))
@@ -13,6 +22,19 @@ mongoose.connect('mongodb+srv://leessang10:WooJoo4032!@mongotest.qwjs5.mongodb.n
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.post('/register', (req, res) => {
+    // 회원가입할 때 필요한 정보를 Client에서 가져와서
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) throw res.json({success: false, err});
+        return res.status(200).json({
+            success: true,
+        });
+    });
+
+    // DB에 넣어준다
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
